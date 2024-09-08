82ed9bda3a3844f7f264a3eba08d106302e0311e
Ivan Lartsov <lartsov@gmail.com>
2024-09-08 22:28:16 +0300

    upd: server-nodejs/src/env.js

--- server-nodejs/src/env.js
+++ server-nodejs/src/env.js
@@ -1,8 +1,11 @@
 import path from "node:path";
 
 import { config as dotenv } from "dotenv-flow";
+import fs from "fs-extra";
 
 dotenv();
 
 process.env.CWD = path.resolve(process.cwd());
 process.env.DATA = path.join(process.env.CWD, ".data");
+
+fs.ensureDirSync(process.env.DATA);
